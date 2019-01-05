643097dc58da05b73df46d9f37254e819a232962
Anne Gerly Ilustre <ag.ilustre@gmail.com>
2019-01-05 14:22:29 +0800

    includes functional view-order-details

--- app/controllers/view_order_details.php
+++ app/controllers/view_order_details.php
@@ -3,6 +3,7 @@
   require 'connect.php';
 
   $id = $_POST['id'];
+  $subTotal = 0;
 
    $data = "
     <table class='table table-hover'>
@@ -15,39 +16,48 @@
         </tr>
       </thead>
       <tbody>";
-
+    
       // Product | Price | Quantity | Sub-total
-  $sql = "SELECT o.id, oi.order_id, oi.quantity, oi.price, i.id, i.name AS product
+  $sql = "SELECT oi.id, oi.quantity, oi.order_id, oi.item_id, o.id, i.id, i.name AS item_name, i.price
           FROM tbl_order_items AS oi
           INNER JOIN tbl_orders AS o
-              ON o.id = oi.order_id
+              ON oi.order_id = o.id
           INNER JOIN tbl_items AS i
-              ON o.item_id = i.id
-          WHERE o.id = '$id'"; 
+              ON oi.item_id = i.id
+          WHERE o.id = $id"; 
 
   $result = mysqli_query($conn, $sql);
+          if (mysqli_num_rows($result) > 0) {
+              while ($row = mysqli_fetch_assoc($result)) {
+                $name = $row["item_name"];
+                $quantity = $row["quantity"];
+                $price = $row["price"];
+                $formatted_price = number_format($price, 2, '.', ',');
 
-   if (mysqli_num_rows($result) == 1) {
-       while ($row = mysqli_fetch_assoc($result)) {
-         $name = $row["product"];
-         $quantity = $row["quantity"];
-         $price = $row["price"];
-         $formatted_price = number_format($price, 2, '.', ',');
+                  //For computing the sub total 
+                  $subTotal = $quantity * $price;
+                  $formatted_subTotal = number_format($subTotal, 2, '.', ',');
 
-           //For computing the sub total 
-           $subTotal = $quantity * $price;
-           $formatted_subTotal = number_format($subTotal, 2, '.', ',');
-
-           $data .=
-             "<tr>
-                 <td>$name</td>
-                 <td>&#x20B1; $formatted_price</td>
-                 <td>$quantity</td>
-                 <td>&#x20B1; $formatted_subTotal</td>
-             </tr>";
-       }
-       $data .="</tbody></table>";
-   } 
+                  $data .=
+                    "<tr>
+                        <td>$name</td>
+                        <td>&#x20B1; $formatted_price</td>
+                        <td class='text-center'>$quantity</td>
+                        <td>&#x20B1; $formatted_subTotal</td>
+                    </tr>";
+              }
+              $data .="</tbody></table>";
+          } 
+          // else {
+          // //to catch the error 
+          //   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+          // }
 
   echo $data;
 ?>
+<!-- 
+  // $sql = "SELECT o.id, oi.id, oi.quantity, oi.price, oi.order_id, oi.item_id
+  //         FROM tbl_orders AS o
+  //         INNER JOIN tbl_order_items AS oi
+  //             ON o.id = oi.order_id
+  //         WHERE o.id = $id";  -->
