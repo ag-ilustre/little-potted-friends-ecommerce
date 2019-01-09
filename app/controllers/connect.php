d4c818a6810bb22cf4d0b08f01ba16742ae134c1
Anne Gerly Ilustre <ag.ilustre@gmail.com>
2019-01-10 03:59:37 +0800

    includes changed connect.php back to db4free

--- app/controllers/connect.php
+++ app/controllers/connect.php
@@ -1,13 +1,13 @@
 <?php
-	$host = "localhost";
-	$db_username = "root";
-	$db_password = "";
-	$db_name = "demoNewStore";
+	// $host = "localhost";
+	// $db_username = "root";
+	// $db_password = "";
+	// $db_name = "demoNewStore";
 	
-	// $host = "db4free.net"; 
-	// $db_username = "agilustre";
-	// $db_password = "csp2ecommerce";
-	// $db_name = "db_csp2_agci";
+	$host = "db4free.net"; 
+	$db_username = "agilustre";
+	$db_password = "csp2ecommerce";
+	$db_name = "db_csp2_agci";
 
 	// Create connection
 	$conn = mysqli_connect($host, $db_username, $db_password, $db_name);
